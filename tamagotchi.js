734515124f1dad308a197cc0409977739047aebf
Jitgitbit <rowowolf@gmail.com>
2020-01-13 20:57:54 +0100

    firts set of functions done

--- tamagotchi.js
+++ tamagotchi.js
@@ -11,7 +11,8 @@ class Tamagotchi {
     this.isIll = false;
     this.isAlive = true;
     this.numberOfPoops = 0;
-    this.lifeCycle = ['baby', 'toddler', 'kid', 'adult'];
+    this.lifeLeft = ['baby', 'child', 'teen', 'adult', 'your pet is DEAD!'];
+    this.lifeCycle = null;
     this.isPottyTrained = false;
     this.gender = ['male', 'female'];
     this.family = 'Dekelver';
@@ -20,20 +21,24 @@ class Tamagotchi {
   hatch() {
     console.log("Our egg is hatching! The miracle of life!");
     this.isHatched = true;
+    this.lifeCycle = this.lifeLeft.shift();
   }
   poop() {
-    console.log('Pooping!');
+    console.log('Pet is pooping!');
     this.numberOfPoops++;
     this.hungerMeter++;
   }
   toilet() {
+    console.log('Pet is potty trained!')
     this.numberOfPoops = 0;
     this.isPottyTrained = true;
   }
   meal() {
+    console.log('A good meal!')
     this.hungerMeter--;
   }
   snack() {
+    console.log('Just a little snack')
     this.hungerMeter--;
     this.happyMeter++;
     const randomNumber = Math.random();
@@ -43,12 +48,30 @@ class Tamagotchi {
     }
     console.log(`Is our pet ill?`, this.isIll);
   }
+  grow() {
+    console.log('Pet is growing up!')
+    this.lifeCycle = this.lifeLeft.shift();
+  }
+  scold() {
+    console.log('Pet has to follow the rules!')
+    this.disciplineMeter++;
+    this.happyMeter--;
+  }
+  play() {}
 }
 const pet = new Tamagotchi('bobo');
 console.log(pet);
 pet.hatch();
 console.log(pet);
-
-//Feed: We should be able to feed our tamagotchi a meal or a snack
-//meal: decreases the hunger meter
-//snack: decreases the hunger meter and increases the happiness meter, but the tamagotchi might become ill
+pet.poop();
+console.log(pet);
+pet.toilet();
+console.log(pet);
+pet.meal();
+console.log(pet);
+pet.snack();
+console.log(pet);
+pet.grow();
+console.log(pet);
+pet.scold();
+console.log(pet);
